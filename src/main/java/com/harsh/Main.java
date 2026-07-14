ecf218b42f1dec528ca8018664a2da7d951949c0
tripathiharsh95 <tripathiharsh088@gmail.com>
2026-07-14 15:18:59 +0530

    Testing Poll SCM

--- src/main/java/com/harsh/Main.java
+++ src/main/java/com/harsh/Main.java
@@ -2,6 +2,6 @@ package com.harsh;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Maven");
+        System.out.println("Jenkins Poll SCM Demo");
     }
 }
